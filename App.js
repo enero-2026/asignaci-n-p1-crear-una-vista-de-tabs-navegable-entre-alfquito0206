36bc811ba4bb8effb7603964b00148ffc710d128
Chopy <alfquito2006@gmail.com>
2026-02-28 11:35:30 -0600

    parcial 1
    tabs

---
+++ App.js
@@ -0,0 +1,26 @@
+
+import { StyleSheet, Text, View, Pressable} from 'react-native';
+import { useRouter } from 'expo-router';
+
+
+export default function App() {
+  const ruta = useRouter();
+
+  return (
+
+    <View style={styles.container}>
+     <Pressable onPress={() => ruta.push('/detalle')}>
+      <Text>Ir a la vista detalle</Text>
+     </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
